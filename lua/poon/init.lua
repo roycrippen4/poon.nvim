6a0bf27c2942146b9901d359be2424032b813fd2
roy.crippen4 <roy.crippen4@archarithms.com>
2025-05-07 15:26:34 -0400

    feat!: User commands for most of the api

--- lua/poon/init.lua
+++ lua/poon/init.lua
@@ -2,10 +2,7 @@ local autocmd = vim.api.nvim_create_autocmd
 local Mark = require('poon.mark')
 local Menu = require('poon.menu')
 
-local M = {
-  mark = {},
-  menu = {},
-}
+local M = {}
 
 local function update_mark(args)
   if Mark.is_marked(args.buf) then
@@ -35,63 +32,91 @@ function M.setup(opts)
 end
 
 ---Add the current file to this project's marks
-function M.mark.set()
+function M.mark_add()
   Mark.set()
+end
+
+---Sets the current buffer as the first mark
+function M.mark_as_first()
+  Mark.set_as_first_mark()
+end
+
+---Removes the current file from the project's marks
+function M.mark_remove()
+  Mark.remove()
 end
 
 ---Navigate to a file.
 ---The `idx` parameter is the index of the file in the menu
 ---@param idx number
-function M.mark.jump(idx)
+function M.jump(idx)
   Mark.jump(idx)
+end
+
+---Get the index for a given mark
+---@param relative_path string
+---@return integer?
+function M.mark_get_index(relative_path)
+  return Mark.idx(relative_path)
 end
 
 ---Get the marks for the current project
 ---@return poon.project.mark[]
-function M.mark.get_marks()
+function M.get_marks()
   return Mark.get_marks()
 end
 
 ---Check if the current file is marked
 ---@param bufnr? integer The bufnr to check. Current buffer is used if not provided
 ---@return boolean
-function M.mark.is_marked(bufnr)
+function M.is_marked(bufnr)
   return Mark.is_marked(bufnr)
-end
-
----Get the index for a given mark
----@param relative_path string
----@return integer?
-function M.mark.get_index(relative_path)
-  return Mark.idx(relative_path)
 end
 
 ---Checks if the current project has marks
 ---@return boolean
-function M.mark.has_marks()
+function M.has_marks()
   return Mark.has_marks()
 end
 
----Removes the current file from the project's marks
-function M.mark.remove()
-  Mark.remove()
-end
-
----Sets the current buffer as the first mark
-function M.mark.set_first()
-  Mark.set_as_first_mark()
-end
-
-function M.menu.open()
+---Open the Poon menu
+function M.menu_open()
   Menu:open()
 end
 
-function M.menu.close()
+---Close the Poon menu
+function M.menu_close()
   Menu:close()
 end
 
-function M.menu.toggle()
+---Toggle the Poon menu
+function M.menu_toggle()
   Menu:toggle()
 end
+
+vim.api.nvim_create_user_command('PoonJump', function(opts)
+  local mark_number = tonumber(opts.args)
+  if not mark_number or mark_number < 0 or math.floor(mark_number) ~= mark_number then
+    M.jump(1)
+    return
+  end
+
+  M.jump(mark_number)
+end, {
+  desc = 'Jump to a mark number. Jumps to the first mark if number is not provided',
+  nargs = '?',
+  complete = function()
+    return vim.tbl_map(tostring, vim.tbl_keys(M.get_marks()))
+  end,
+})
+
+vim.api.nvim_create_user_command('PoonAddMark', function(opts)
+  (opts.bang and M.mark_as_first or M.mark_add)()
+end, { desc = 'Add file to marks. Use `!` to set as first mark', bang = true })
+
+vim.api.nvim_create_user_command('PoonRemoveMark', M.mark_remove, { desc = 'Remove the current file from the marks list' })
+vim.api.nvim_create_user_command('PoonToggle', M.menu_toggle, { desc = 'Toggle the Poon Menu' })
+vim.api.nvim_create_user_command('PoonOpen', M.menu_open, { desc = 'Open the Poon Menu' })
+vim.api.nvim_create_user_command('PoonClose', M.menu_close, { desc = 'Open the Poon Menu' })
 
 return M
